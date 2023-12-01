9e992c8a72c619fe2dc141a6ab8c82227779c427
Dominik Furman <s222915@student.uek.krakw.pl>
2023-12-01 18:36:58 +0100

    create, reas, update, delete implemented

--- src/main/java/pl/dfurman/voter/VoterController.java
+++ src/main/java/pl/dfurman/voter/VoterController.java
@@ -1,10 +1,9 @@
 package pl.dfurman.voter;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
@@ -25,5 +24,17 @@ public class VoterController {
     @PostMapping
     public void registerNewVoter(@RequestBody Voter voter) {
         voterService.addNewVoter(voter);
+    }
+
+    @DeleteMapping(path = "{voterId}")
+    public void deleteVoter(@PathVariable("voterId") Long voterId) {
+        voterService.deleteVoter(voterId);
+    }
+
+    @PutMapping(path = "{voterId}")
+    public void updateVoter(@PathVariable("voterId") Long voterId,
+                            @RequestParam(required = false) String firstName,
+                            @RequestParam(required = false) String email) {
+        voterService.updateVoter(voterId, firstName, email);
     }
 }
--- src/main/java/pl/dfurman/voter/VoterService.java
+++ src/main/java/pl/dfurman/voter/VoterService.java
@@ -1,12 +1,12 @@
 package pl.dfurman.voter;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -29,5 +29,31 @@ public class VoterService {
             throw new IllegalStateException("email taken");
         }
         voterRepository.save(voter);
+    }
+
+    public void deleteVoter(Long voterId) {
+        boolean exists = voterRepository.existsById(voterId);
+        if (!exists) {
+            throw new IllegalStateException("Voter with id " + voterId + " does not exist");
+        }
+        voterRepository.deleteById(voterId);
+    }
+
+    @Transactional
+    public void updateVoter(Long voterId, String firstName, String email) {
+        Voter voter = voterRepository.findById(voterId).orElseThrow(
+                () -> new IllegalStateException("voter with id " + voterId + " not found")
+        );
+        if (firstName != null && !firstName.isEmpty() && !Objects.equals(voter.getFirstName(), firstName)) {
+            voter.setFirstName(firstName);
+        }
+        if (email != null && !email.isEmpty() && !Objects.equals(voter.getEmail(), email)) {
+            Optional<Voter> voterOptional = voterRepository.findByEmail(email);
+            if (voterOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            voter.setEmail(email);
+        }
+
     }
 }
